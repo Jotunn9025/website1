40437df94aef3ad12c4899a91c776de7ac493d5a
Jotunn9025 <youhanlalwani@gmail.com>
2024-03-26 00:09:28 +0530

    commits w/o history

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple website about myself\
+has only one commit duese to me deleting previous one as there was sensitive info in commit history
---
+++ escript.js
@@ -0,0 +1,54 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let interval = null;
+  
+  document.querySelectorAll(".esidenav a").forEach(anchor => {
+    anchor.onmouseover = event => {  
+      let iteration = 0;
+      clearInterval(interval);
+  
+      interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if(index < iteration) {
+              return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)];
+          })
+          .join("");
+        
+        if(iteration >= event.target.dataset.value.length){ 
+          clearInterval(interval);
+        }
+        
+        iteration += 1 /3;
+      }, 30);
+      
+    }
+    anchor.addEventListener('mouseleave', event => {
+      clearInterval(interval);
+      event.target.innerText = event.target.dataset.value;
+  });
+  });
+  });
+  function toggleNav() {
+    var sidenav = document.getElementById("sidenav");
+    if (sidenav.style.width === "250px") {
+        sidenav.style.width = "0";
+    } else {
+        sidenav.style.width = "250px";
+    }
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
+    body.addEventListener('mousemove', (e) => {
+        const maxX = window.innerWidth;
+        const bgWidth = body.offsetWidth;
+        const x = e.clientX;
+        const percent = (x / maxX) * 100;
+        const bgPosition = -((bgWidth - maxX) * percent / 100);
+        body.style.backgroundPositionX = `${bgPosition}px`;
+    });
+  });
